90dba43bfc05211b2b34e5d5e21ad414339380a0
Bablgumz <aleksandrsviridov824@gmail.com>
2021-02-11 20:41:37 +0300

    .

--- src/lesson_18/Main.java
+++ src/lesson_18/Main.java
@@ -2,7 +2,71 @@ package lesson_18;
 
 public class Main {
 
+    volatile int status = 0;
+
     public static void main(String[] args) {
-	// write your code here
+        Main lock = new Main();
+        Thread t1 = new Thread(new Task("A", lock, 1));
+        Thread t2 = new Thread(new Task("B", lock, 2));
+        Thread t3 = new Thread(new Task("C", lock, 3));
+        t1.start();
+        t2.start();
+        t3.start();
+
     }
+}
+
+class Task implements Runnable {
+
+    private String massage;
+    private final Main lock = new Main();
+    private int p;
+    private boolean message;
+
+    Task(String massage, Main lock) {
+        this.massage = massage;
+    }
+
+    public Task(String a, Main lock, int i) {
+
+    }
+
+
+    @Override
+public void run() {
+
+    while (lock.status < 13) {
+        synchronized (lock) {
+
+            while (!((lock.status % 3) == 0 ) && p ==1) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            while (!((lock.status % 3) == 1) && p == 2){
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            while(!((lock.status % 3) == 2) && p == 3){
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            System.out.println(message);
+            lock.status++;
+            if ((lock.status % 3) == 0) System.out.println("---");
+            lock.notifyAll();
+        }
+    }
+}
 }
